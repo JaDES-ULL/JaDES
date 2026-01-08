8d4a795443840bcb07f2169cf02a3cd8c4fb2028
Miqueas Garcia <mqggonzalez@gmail.com>
2026-01-08 15:14:35 +0000

    test(variable): add comprehensive tests for DoubleVariable and CharacterVariable classes

---
+++ jades-core/src/test/java/es/ull/simulation/variable/CharacterVariableTest.java
@@ -0,0 +1,246 @@
+package es.ull.simulation.variable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the CharacterVariable class
+ */
+class CharacterVariableTest {
+
+    @Test
+    void shouldCreateVariableFromCharacter() {
+        // Given: a Character value
+        // When: creating a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('A');
+        
+        // Then: it should return character count (always 1)
+        assertEquals(Integer.valueOf(1), variable.getValue());
+    }
+    
+    @Test
+    void shouldCreateVariableFromLowercaseLetter() {
+        // Given: a lowercase letter
+        // When: creating a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('z');
+        
+        // Then: it should store the character
+        assertEquals(Integer.valueOf(1), variable.getValue());
+        assertEquals("z", variable.toString());
+    }
+    
+    @Test
+    void shouldCreateVariableFromDigit() {
+        // Given: a digit character
+        // When: creating a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('5');
+        
+        // Then: it should store the digit
+        assertEquals("5", variable.toString());
+    }
+    
+    @Test
+    void shouldCreateVariableFromSpecialChar() {
+        // Given: a special character
+        // When: creating a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('$');
+        
+        // Then: it should store the special char
+        assertEquals("$", variable.toString());
+    }
+    
+    @Test
+    void shouldSetValueFromObject() {
+        // Given: a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('A');
+        
+        // When: setting value from Character object
+        variable.setValue(Character.valueOf('Z'));
+        
+        // Then: it should update the value
+        assertEquals("Z", variable.toString());
+    }
+    
+    @Test
+    void shouldSetValueFromInt() {
+        // Given: a CharacterVariable
+        // When: setting value from int (ASCII 65 = 'A')
+        CharacterVariable variable = new CharacterVariable('x');
+        variable.setValue(65);
+        
+        // Then: it should convert to character
+        assertEquals("A", variable.toString());
+    }
+    
+    @Test
+    void shouldSetValueFromTrue() {
+        // Given: a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('A');
+        
+        // When: setting value from boolean true
+        variable.setValue(true);
+        
+        // Then: it should be '0'
+        assertEquals("0", variable.toString());
+    }
+    
+    @Test
+    void shouldSetValueFromFalse() {
+        // Given: a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('A');
+        
+        // When: setting value from boolean false
+        variable.setValue(false);
+        
+        // Then: it should be '1'
+        assertEquals("1", variable.toString());
+    }
+    
+    @Test
+    void shouldSetValueFromChar() {
+        // Given: a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('A');
+        
+        // When: setting value from char
+        variable.setValue('B');
+        
+        // Then: it should update the character
+        assertEquals("B", variable.toString());
+    }
+    
+    @Test
+    void shouldSetValueFromByte() {
+        // Given: a CharacterVariable
+        // When: setting value from byte (66 = 'B')
+        CharacterVariable variable = new CharacterVariable('A');
+        variable.setValue((byte) 66);
+        
+        // Then: it should convert to character
+        assertEquals("B", variable.toString());
+    }
+    
+    @Test
+    void shouldSetValueFromDouble() {
+        // Given: a CharacterVariable
+        // When: setting value from double (67.9 truncates to 67 = 'C')
+        CharacterVariable variable = new CharacterVariable('A');
+        variable.setValue(67.9);
+        
+        // Then: it should truncate and convert to character
+        assertEquals("C", variable.toString());
+    }
+    
+    @Test
+    void shouldSetValueFromFloat() {
+        // Given: a CharacterVariable
+        // When: setting value from float (68.5 truncates to 68 = 'D')
+        CharacterVariable variable = new CharacterVariable('A');
+        variable.setValue(68.5f);
+        
+        // Then: it should truncate and convert to character
+        assertEquals("D", variable.toString());
+    }
+    
+    @Test
+    void shouldSetValueFromLong() {
+        // Given: a CharacterVariable
+        // When: setting value from long (69 = 'E')
+        CharacterVariable variable = new CharacterVariable('A');
+        variable.setValue(69L);
+        
+        // Then: it should convert to character
+        assertEquals("E", variable.toString());
+    }
+    
+    @Test
+    void shouldSetValueFromShort() {
+        // Given: a CharacterVariable
+        // When: setting value from short (70 = 'F')
+        CharacterVariable variable = new CharacterVariable('A');
+        variable.setValue((short) 70);
+        
+        // Then: it should convert to character
+        assertEquals("F", variable.toString());
+    }
+    
+    @Test
+    void shouldReturnCorrectToString() {
+        // Given: a CharacterVariable with '@'
+        CharacterVariable variable = new CharacterVariable('@');
+        
+        // When: calling toString
+        String result = variable.toString();
+        
+        // Then: it should return "@"
+        assertEquals("@", result);
+    }
+    
+    @Test
+    void shouldHandleSpace() {
+        // Given: a space character
+        // When: creating a CharacterVariable
+        CharacterVariable variable = new CharacterVariable(' ');
+        
+        // Then: it should store the space
+        assertEquals(" ", variable.toString());
+    }
+    
+    @Test
+    void shouldHandleNewline() {
+        // Given: a newline character
+        // When: creating a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('\n');
+        
+        // Then: it should store the newline
+        assertEquals("\n", variable.toString());
+    }
+    
+    @Test
+    void shouldHandleTab() {
+        // Given: a tab character
+        // When: creating a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('\t');
+        
+        // Then: it should store the tab
+        assertEquals("\t", variable.toString());
+    }
+    
+    @Test
+    void shouldHandleZeroChar() {
+        // Given: null character (ASCII 0)
+        // When: creating a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('\0');
+        
+        // Then: it should store the null char
+        assertEquals("\0", variable.toString());
+    }
+    
+    @Test
+    void shouldUpdateValue_whenSetMultipleTimes() {
+        // Given: a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('A');
+        
+        // When: setting value multiple times
+        variable.setValue('B');
+        assertEquals("B", variable.toString());
+        variable.setValue('C');
+        assertEquals("C", variable.toString());
+        variable.setValue('D');
+        
+        // Then: it should have the final value
+        assertEquals("D", variable.toString());
+    }
+    
+    @Test
+    void shouldHandleUnicodeCharacter() {
+        // Given: a Unicode character
+        // When: creating a CharacterVariable
+        CharacterVariable variable = new CharacterVariable('€');
+        
+        // Then: it should store the Unicode character
+        assertEquals("€", variable.toString());
+    }
+}
---
+++ jades-core/src/test/java/es/ull/simulation/variable/DoubleVariableTest.java
@@ -0,0 +1,299 @@
+package es.ull.simulation.variable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the DoubleVariable class
+ */
+class DoubleVariableTest {
+
+    @Test
+    void shouldCreateVariableFromDouble() {
+        // Given: a double value
+        // When: creating a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(3.14159);
+        
+        // Then: it should return the correct value
+        assertEquals(Double.valueOf(3.14159), variable.getValue());
+    }
+    
+    @Test
+    void shouldCreateVariableFromZero() {
+        // Given: zero value
+        // When: creating a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(0.0);
+        
+        // Then: it should store zero
+        assertEquals(Double.valueOf(0.0), variable.getValue());
+    }
+    
+    @Test
+    void shouldCreateVariableFromNegative() {
+        // Given: a negative double value
+        // When: creating a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(-99.99);
+        
+        // Then: it should store the negative value
+        assertEquals(Double.valueOf(-99.99), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromInt() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.5);
+        
+        // When: setting value from int
+        variable.setValue(42);
+        
+        // Then: it should convert to double
+        assertEquals(Double.valueOf(42.0), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromTrue() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.0);
+        
+        // When: setting value from boolean true
+        variable.setValue(true);
+        
+        // Then: it should be 0
+        assertEquals(Double.valueOf(0.0), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromFalse() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.0);
+        
+        // When: setting value from boolean false
+        variable.setValue(false);
+        
+        // Then: it should be 1
+        assertEquals(Double.valueOf(1.0), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromChar() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.0);
+        
+        // When: setting value from char 'A' (ASCII 65)
+        variable.setValue('A');
+        
+        // Then: it should convert to double of ASCII value
+        assertEquals(Double.valueOf(65.0), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromByte() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.0);
+        
+        // When: setting value from byte
+        variable.setValue((byte) 127);
+        
+        // Then: it should convert to double
+        assertEquals(Double.valueOf(127.0), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromDouble() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.0);
+        
+        // When: setting value from double
+        variable.setValue(99.99);
+        
+        // Then: it should update the value
+        assertEquals(Double.valueOf(99.99), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromFloat() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.0);
+        
+        // When: setting value from float
+        variable.setValue(15.5f);
+        
+        // Then: it should convert to double
+        assertEquals(Double.valueOf(15.5), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromLong() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.0);
+        
+        // When: setting value from long
+        variable.setValue(1000000L);
+        
+        // Then: it should convert to double
+        assertEquals(Double.valueOf(1000000.0), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromShort() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.0);
+        
+        // When: setting value from short
+        variable.setValue((short) 500);
+        
+        // Then: it should convert to double
+        assertEquals(Double.valueOf(500.0), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromNumber() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.0);
+        
+        // When: setting value from Number object
+        variable.setValue(Double.valueOf(3.14159));
+        
+        // Then: it should update the value
+        assertEquals(Double.valueOf(3.14159), variable.getValue());
+    }
+    
+    @Test
+    void shouldSetValueFromObject() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(10.0);
+        
+        // When: setting value from Object (Number)
+        variable.setValue((Object) Double.valueOf(2.71828));
+        
+        // Then: it should update the value
+        assertEquals(Double.valueOf(2.71828), variable.getValue());
+    }
+    
+    @Test
+    void shouldReturnCorrectToString() {
+        // Given: a DoubleVariable with value 3.14
+        DoubleVariable variable = new DoubleVariable(3.14);
+        
+        // When: calling toString
+        String result = variable.toString();
+        
+        // Then: it should return "3.14"
+        assertEquals("3.14", result);
+    }
+    
+    @Test
+    void shouldCompareEqualVariables() {
+        // Given: two DoubleVariables with same value
+        DoubleVariable var1 = new DoubleVariable(3.14159);
+        DoubleVariable var2 = new DoubleVariable(3.14159);
+        
+        // When: comparing them
+        boolean result = var1.equals(var2);
+        
+        // Then: they should be equal
+        assertTrue(result);
+    }
+    
+    @Test
+    void shouldCompareDifferentVariables() {
+        // Given: two DoubleVariables with different values
+        DoubleVariable var1 = new DoubleVariable(3.14);
+        DoubleVariable var2 = new DoubleVariable(2.71);
+        
+        // When: comparing them
+        boolean result = var1.equals(var2);
+        
+        // Then: they should not be equal
+        assertFalse(result);
+    }
+    
+    @Test
+    void shouldHandleMaxValue() {
+        // Given: maximum double value
+        // When: creating a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(Double.MAX_VALUE);
+        
+        // Then: it should store the max value
+        assertEquals(Double.valueOf(Double.MAX_VALUE), variable.getValue());
+    }
+    
+    @Test
+    void shouldHandleMinValue() {
+        // Given: minimum double value
+        // When: creating a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(Double.MIN_VALUE);
+        
+        // Then: it should store the min value
+        assertEquals(Double.valueOf(Double.MIN_VALUE), variable.getValue());
+    }
+    
+    @Test
+    void shouldHandlePositiveInfinity() {
+        // Given: positive infinity
+        // When: creating a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(Double.POSITIVE_INFINITY);
+        
+        // Then: it should store positive infinity
+        assertEquals(Double.valueOf(Double.POSITIVE_INFINITY), variable.getValue());
+    }
+    
+    @Test
+    void shouldHandleNegativeInfinity() {
+        // Given: negative infinity
+        // When: creating a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(Double.NEGATIVE_INFINITY);
+        
+        // Then: it should store negative infinity
+        assertEquals(Double.valueOf(Double.NEGATIVE_INFINITY), variable.getValue());
+    }
+    
+    @Test
+    void shouldHandleVerySmallNumber() {
+        // Given: a very small number
+        // When: creating a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(0.000000001);
+        
+        // Then: it should store the small value
+        assertEquals(Double.valueOf(0.000000001), variable.getValue());
+    }
+    
+    @Test
+    void shouldHandleVeryLargeNumber() {
+        // Given: a very large number
+        // When: creating a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(1.23456789e100);
+        
+        // Then: it should store the large value
+        assertEquals(Double.valueOf(1.23456789e100), variable.getValue());
+    }
+    
+    @Test
+    void shouldUpdateValue_whenSetMultipleTimes() {
+        // Given: a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(1.0);
+        
+        // When: setting value multiple times
+        variable.setValue(2.0);
+        assertEquals(Double.valueOf(2.0), variable.getValue());
+        variable.setValue(3.0);
+        assertEquals(Double.valueOf(3.0), variable.getValue());
+        variable.setValue(4.0);
+        
+        // Then: it should have the final value
+        assertEquals(Double.valueOf(4.0), variable.getValue());
+    }
+    
+    @Test
+    void shouldHandleNegativeZero() {
+        // Given: negative zero
+        // When: creating a DoubleVariable
+        DoubleVariable variable = new DoubleVariable(-0.0);
+        
+        // Then: it should store negative zero
+        assertEquals(Double.valueOf(-0.0), variable.getValue());
+    }
+}
